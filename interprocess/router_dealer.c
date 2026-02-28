0a3b91ad9047eb3302ffe25a2e988e063ddac556
Midas5665 <portocale2017@gmail.com>
2026-02-28 12:58:39 -0800

    Added helperFunction getAttr that prints the properties of a queue(Useful for debugging). Created message queues for dealer reading from client, dealer to workers and dealer reading from workers

--- interprocess/router_dealer.c
+++ interprocess/router_dealer.c
@@ -34,6 +34,23 @@ char dealer2worker1_name[30];
 char dealer2worker2_name[30];
 char worker2dealer_name[30];
 
+static void 
+getattr (mqd_t mq_fd)
+{
+    struct mq_attr      attr;
+    int                 rtnval;
+    
+    rtnval = mq_getattr (mq_fd, &attr);
+    if (rtnval == -1)
+    {
+        perror ("mq_getattr() failed");
+        exit (1);
+    }
+    fprintf (stderr, "%d: mqdes=%d max=%ld size=%ld nrof=%ld\n",
+                getpid(), 
+                mq_fd, attr.mq_maxmsg, attr.mq_msgsize, attr.mq_curmsgs);
+}
+
 int main (int argc, char * argv[])
 {
   if (argc != 1)
@@ -45,6 +62,47 @@ int main (int argc, char * argv[])
   // TODO:
     //  * create the message queues (see message_queue_test() in
     //    interprocess_basic.c)
+  mqd_t mq_fd_req;  //Defining the file descriptors for request, response, and worker queues
+  mqd_t mq_fd_rep;
+  mqd_t mq_fd_S1;
+  mqd_t mq_fd_S2;
+
+  REQ_MESSAGE req; //Instantiating one type of every message type, might be useful later
+  RSP_MESSAGE rsp;
+  SERVICE_MESSAGE ser;
+
+  struct mq_attr attr; //Instantiating structure of attributes for message queues
+
+  sprintf (client2dealer_name, "/mq_request_%s_%d", "Group44", getpid()); //Naming the file paths to the queues according to the 
+  sprintf (dealer2worker1_name, "/mq_worker1_%s_%d", "Group44", getpid()); //assignment specificiation. Our group number is included
+  sprintf (dealer2worker2_name, "/mq_worker2_%s_%d", "Group44", getpid());
+  sprintf (worker2dealer_name, "/mq_response_%s_%d", "Group44", getpid());
+
+  attr.mq_maxmsg = MQ_MAX_MESSAGES; //Number of messages in a queue is constant across the board
+
+  //Dealer can only read from the queue in which the client sends
+  attr.mq_msgsize = sizeof(REQ_MESSAGE);
+  mq_fd_req = mq_open (client2dealer_name, O_RDONLY | O_CREAT | O_EXCL, 0600, &attr); //Opening a queue for requests
+  
+  //Dealer can only read from the response queue in which the workers send
+  attr.mq_msgsize = sizeof(RSP_MESSAGE);
+  mq_fd_rep = mq_open (worker2dealer_name, O_RDONLY | O_CREAT | O_EXCL, 0600, &attr); //Opening a queue for worker responses
+
+  //Dealer can only write in the queue towards the workers, sending services their way
+  attr.mq_msgsize = sizeof(SERVICE_MESSAGE);
+  mq_fd_S1 = mq_open(dealer2worker1_name, O_WRONLY | O_CREAT | O_EXCL, 0600, &attr);  //Opening queue for service 1 tasks
+  mq_fd_S2 = mq_open(dealer2worker2_name, O_WRONLY | O_CREAT | O_EXCL, 0600, &attr);  //Opening queue for service 2 tasks
+
+  //Debugging function, making sure all queues are defined as we want them
+  getattr(mq_fd_rep);
+  getattr(mq_fd_req);
+  getattr(mq_fd_S1);
+  getattr(mq_fd_S2);
+
+
+
+
+
     //  * create the child processes (see process_test() and
     //    message_queue_test())
     //  * read requests from the Req queue and transfer them to the workers

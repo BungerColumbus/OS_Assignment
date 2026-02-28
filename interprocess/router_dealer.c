1b4b8166a21304e9a4b0e5a4701c059f6fcbeaae
Midas5665 <portocale2017@gmail.com>
2026-02-28 15:40:41 -0800

    Added client and worker process creation process. Added shortcut for paths to the worker and client files

--- interprocess/router_dealer.c
+++ interprocess/router_dealer.c
@@ -34,6 +34,7 @@ char dealer2worker1_name[30];
 char dealer2worker2_name[30];
 char worker2dealer_name[30];
 
+//Helper function for printing the attributes of a function
 static void 
 getattr (mqd_t mq_fd)
 {
@@ -62,6 +63,14 @@ int main (int argc, char * argv[])
   // TODO:
     //  * create the message queues (see message_queue_test() in
     //    interprocess_basic.c)
+
+
+  //Pointers towards the filenames of the programs that will be run by the child processes
+  char* worker1Path = "/home/student/Documents/OS_Assignment/interprocess/worker_s1.c"; 
+  char* clientPath = "/home/student/Documents/OS_Assignment/interprocess/client.c";
+  char* worker2Path = "/home/student/Documents/OS_Assignment/interprocess/worker_s2.c";
+
+  pid_t processID;  //Defining process ID for the router dealer
   mqd_t mq_fd_req;  //Defining the file descriptors for request, response, and worker queues
   mqd_t mq_fd_rep;
   mqd_t mq_fd_S1;
@@ -100,13 +109,71 @@ int main (int argc, char * argv[])
   getattr(mq_fd_S2);
 
 
-
-
-
     //  * create the child processes (see process_test() and
     //    message_queue_test())
+
+  processID = fork();
+  if (processID < 0) {
+    perror("client fork() failed");  //Check if the fork failed
+    exit(1);
+  } else
+    {   
+      //Assign client process its main function
+      if (processID == 0)
+      {
+          execlp(*clientPath, *clientPath, mq_fd_req, NULL);
+
+          perror("execlp failed"); //The program should never reach here
+          exit(2);
+      }
+
+      //Assign worker 1 processes their main function. There should be NSERV1 of them
+      //First we create NSERV1 of them. Only the parent process can do that
+      for (int i=0; i < N_SERV1; i++){
+        if (processID > 0){
+          processID = fork();
+        }
+      }
+      if(processID < 0){
+        perror("worker fork() failed");  //Check if the fork failed
+        exit(1);
+      }
+
+      if(processID == 0){
+        execlp(*worker1Path, *worker1Path, mq_fd_S1, mq_fd_rep, NULL);
+        
+        perror("execlp failed");  //The program should never reach here
+        exit(2);
+      }
+
+
+      //Assign worker 2 processes their main function. There should be NSERV2 of them
+      //First we create NSERV2 of them. Only the parent process can do that
+      for (int i=0; i < N_SERV2; i++){
+        if (processID > 0){
+          processID = fork();
+        }
+      }
+      if(processID < 0){
+        perror("worker fork() failed");  //Check if the fork failed
+        exit(1);
+      }
+
+      if(processID == 0){
+        execlp(*worker2Path, *worker2Path, mq_fd_S1, mq_fd_rep, NULL);
+        
+        perror("execlp failed");  //The program should never reach here
+        exit(2);
+      }
+
     //  * read requests from the Req queue and transfer them to the workers
     //    with the Sx queues
+
+
+    }
+
+
+
     //  * read answers from workers in the Rep queue and print them
     //  * wait until the client has been stopped (see process_test())
     //  * clean up the message queues (see message_queue_test())
